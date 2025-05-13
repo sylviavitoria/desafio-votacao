0d8ddb7fbfe9a00653e552af34fbb7e8733b4818
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-13 16:27:33 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/com/sylviavitoria/api_votacao/ApiVotacaoApplication.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.api_votacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVotacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVotacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sylviavitoria/api_votacao/ApiVotacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sylviavitoria.api_votacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVotacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
