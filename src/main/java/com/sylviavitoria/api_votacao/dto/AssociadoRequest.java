2af563f6dee2e41b7bdaf347788afd3bdca11bcd
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-15 11:09:05 -0300

    feat(crud-votacao): criar classe AssociadoDTO e refatorar AssociadoRequest para usar @Data #2

--- src/main/java/com/sylviavitoria/api_votacao/dto/AssociadoRequest.java
+++ src/main/java/com/sylviavitoria/api_votacao/dto/AssociadoRequest.java
@@ -4,11 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+
+@Data
 @Schema(description = "Dados para criação ou atualização de um associado")
 public class AssociadoRequest {
 
