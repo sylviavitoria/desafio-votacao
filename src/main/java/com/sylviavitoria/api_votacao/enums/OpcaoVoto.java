c02ccea71b80a877d4d31546c0085e6d1640ab9b
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-16 19:19:40 -0300

    feat(crud-voto-e-sessao): adicionar enums OpcaoVoto e StatusSessao para gerenciamento de votação #6

---
+++ src/main/java/com/sylviavitoria/api_votacao/enums/OpcaoVoto.java
@@ -0,0 +1,6 @@
+package com.sylviavitoria.api_votacao.enums;
+
+public enum OpcaoVoto {
+    SIM,
+    NAO
+}
---
+++ src/main/java/com/sylviavitoria/api_votacao/enums/StatusSessao.java
@@ -0,0 +1,7 @@
+package com.sylviavitoria.api_votacao.enums;
+
+public enum StatusSessao {
+    ABERTA,    
+    FECHADA,   
+    FINALIZADA
+}
