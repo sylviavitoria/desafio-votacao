ab5385b356fde22e81b31304402296bdedcfa379
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-15 23:06:54 -0300

    feat(crud-pauta): adicionar métodos de atualização e deleção na interface IPauta #4

--- src/main/java/com/sylviavitoria/api_votacao/interfaces/IPauta.java
+++ src/main/java/com/sylviavitoria/api_votacao/interfaces/IPauta.java
@@ -1,10 +1,12 @@
 package com.sylviavitoria.api_votacao.interfaces;
 
+import com.sylviavitoria.api_votacao.dto.PautaAtualizarRequest;
 import com.sylviavitoria.api_votacao.dto.PautaRequest;
 import com.sylviavitoria.api_votacao.dto.PautaResponse;
 
 public interface IPauta {
     PautaResponse criar(PautaRequest request);
     PautaResponse buscarPorId(Long id);
-    
+    PautaResponse atualizar(Long id, PautaAtualizarRequest request);
+    void deletar(Long id);
 }
