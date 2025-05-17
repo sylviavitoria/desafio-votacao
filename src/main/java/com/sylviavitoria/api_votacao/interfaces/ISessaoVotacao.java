6ce4174e0d03ad1d2bac032e05910a12b4c541f9
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-17 17:20:42 -0300

    feat(crud-voto-e-sessao): adicionar métodos de atualização e listagem nas interfaces ISessaoVotacao e IVoto #6

--- src/main/java/com/sylviavitoria/api_votacao/interfaces/ISessaoVotacao.java
+++ src/main/java/com/sylviavitoria/api_votacao/interfaces/ISessaoVotacao.java
@@ -1,9 +1,14 @@
 package com.sylviavitoria.api_votacao.interfaces;
 
+import com.sylviavitoria.api_votacao.dto.SessaoVotacaoAtualizarRequest;
 import com.sylviavitoria.api_votacao.dto.SessaoVotacaoRequest;
 import com.sylviavitoria.api_votacao.dto.SessaoVotacaoResponse;
+
+import org.springframework.data.domain.Page;
 
 public interface ISessaoVotacao {
     SessaoVotacaoResponse criar(SessaoVotacaoRequest request);
     SessaoVotacaoResponse buscarPorId(Long id);
+    Page<SessaoVotacaoResponse> listarTodos(int page, int size, String sort);
+    SessaoVotacaoResponse atualizarPeriodo(Long id, SessaoVotacaoAtualizarRequest request);
 }
--- src/main/java/com/sylviavitoria/api_votacao/interfaces/IVoto.java
+++ src/main/java/com/sylviavitoria/api_votacao/interfaces/IVoto.java
@@ -1,10 +1,14 @@
 package com.sylviavitoria.api_votacao.interfaces;
 
 import com.sylviavitoria.api_votacao.dto.ResultadoVotacaoResponse;
+import com.sylviavitoria.api_votacao.dto.VotoAtualizarRequest;
 import com.sylviavitoria.api_votacao.dto.VotoRequest;
 import com.sylviavitoria.api_votacao.dto.VotoResponse;
 
+
 public interface IVoto {
     VotoResponse registrarVoto(VotoRequest request);
+    VotoResponse buscarPorId(Long id); 
     ResultadoVotacaoResponse consultarResultado(Long pautaId);
+    VotoResponse atualizarVoto(Long id, VotoAtualizarRequest request);
 }
