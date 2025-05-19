61dded26cf1fe61e8bc249abc98ba203c64f0bbd
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-05-18 22:12:15 -0300

    feat(crud-voto-e-sessao): adicionar construtores padrão e com argumentos #6

--- src/main/java/com/sylviavitoria/api_votacao/dto/VotoResponse.java
+++ src/main/java/com/sylviavitoria/api_votacao/dto/VotoResponse.java
@@ -3,11 +3,16 @@ package com.sylviavitoria.api_votacao.dto;
 import java.time.LocalDateTime;
 
 import com.sylviavitoria.api_votacao.enums.OpcaoVoto;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VotoResponse {
     
     private Long id;
