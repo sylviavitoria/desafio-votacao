2ae9d3d8ffeffb665d33393b8c7bad30cc22977a
sylviavitoria <sylvia.souza@dbserver.com.br>
2025-06-22 18:32:23 -0300

    refactor: limpar código da configuração CORS

--- src/main/java/com/sylviavitoria/api_votacao/config/CorsConfig.java
+++ src/main/java/com/sylviavitoria/api_votacao/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173") // Endereço do seu front Vite
+                .allowedOrigins("http://localhost:5173") 
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
